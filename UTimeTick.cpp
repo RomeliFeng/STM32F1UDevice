af4289a2d077c4846b9e22378d9949af2e1f42f3
romeli <fengzheng1995@gmail.com>
2018-03-01 14:06:28 +0800

    定时检测函数

---
+++ UTimeTick.cpp
@@ -0,0 +1,54 @@
+/*
+ * UTimeTick.cpp
+ *
+ *  Created on: 2017��1��11��
+ *      Author: Romeli
+ */
+
+#include <UTimeTick.h>
+
+UTimeTick::UTimeTick(TIM_TypeDef* TIMx, UIT_Typedef& it) {
+	_TIMx = TIMx;
+	_IT = it;
+}
+
+void UTimeTick::Init(uint16_t ms) {
+	TIMInit(ms);
+	NVICInit();
+}
+
+void UTimeTick::TIMInit(uint16_t ms) {
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+	TIMRCCInit();
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseStructure.TIM_Prescaler = (SystemCoreClock >> 1) / 1000 - 1;
+	TIM_TimeBaseStructure.TIM_Period = ms << 1;
+	TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+	TIM_TimeBaseInit(_TIMx, &TIM_TimeBaseStructure);
+}
+
+void UTimeTick::NVICInit() {
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	NVIC_InitStructure.NVIC_IRQChannel = _IT.NVIC_IRQChannel;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority =
+			_IT.PreemptionPriority;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = _IT.SubPriority;
+	NVIC_Init(&NVIC_InitStructure);
+
+	TIM_ITConfig(_TIMx, TIM_IT_Update, ENABLE);
+
+	TIM_Cmd(_TIMx, ENABLE);
+}
+#include "stdio.h"
+
+void UTimeTick::ISR() {
+	if ((_TIMx->SR & TIM_IT_Update) != 0) {
+		TryDo();
+		_TIMx->SR = (uint16_t) ~TIM_IT_Update;
+	}
+}
+
---
+++ UTimeTick.h
@@ -0,0 +1,29 @@
+/*
+ * TimeTick.h
+ *
+ *  Created on: 2017��1��11��
+ *      Author: Romeli
+ */
+
+#ifndef UTIMETICK_H_
+#define UTIMETICK_H_
+
+#include <cmsis_device.h>
+#include <Event/UEventLoop.h>
+
+class UTimeTick: public UEventLoop {
+public:
+	UTimeTick(TIM_TypeDef* TIMx, UIT_Typedef& it);
+	void Init(uint16_t ms);
+	void ISR();
+protected:
+	TIM_TypeDef* _TIMx;	//响应时间计算用定时器
+	UIT_Typedef _IT; //中断优先级
+
+	virtual void TIMRCCInit() = 0;
+private:
+	void TIMInit(uint16_t ms);
+	void NVICInit();
+};
+
+#endif /* UTIMETICK_H_ */
